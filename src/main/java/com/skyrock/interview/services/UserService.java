20a44810a915ff3370c630ddc50823800c4cf90d
adrian.mutize@qubedlab.com <adrian.mutize@qubedlab.com>
2021-09-23 20:33:34 +0200

    removed unused imports

--- src/main/java/com/skyrock/interview/services/UserService.java
+++ src/main/java/com/skyrock/interview/services/UserService.java
@@ -5,7 +5,6 @@ import com.skyrock.interview.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
