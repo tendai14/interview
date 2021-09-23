ac33f167224eda20ff6d9f242fe7e12e3acc4715
adrian.mutize@qubedlab.com <adrian.mutize@qubedlab.com>
2021-09-23 22:40:33 +0200

    cleaned up swagger config class

--- src/main/java/com/skyrock/interview/config/SwaggerUiConfiguration.java
+++ src/main/java/com/skyrock/interview/config/SwaggerUiConfiguration.java
@@ -3,6 +3,7 @@ package com.skyrock.interview.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,20 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerUiConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public Docket api() {
-        // @formatter:off
-        //Register the controllers to swagger
-        //Also it is configuring the Swagger Docket
+
         return new Docket(DocumentationType.SWAGGER_2).select()
-//                 .apis(RequestHandlerSelectors.any())
                  .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-//                 .paths(PathSelectors.any())
-//                 .paths(PathSelectors.ant("/swagger2-demo"))
                 .build();
-        // @formatter:on
+
     }
 
     @Override
